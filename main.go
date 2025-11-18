80bfef0b7f3936ee31a13c6a3d89a0aad454d108
Towbee05 <olatiseoluwatobiloba@gmail.com>
2025-11-18 23:59:34 +0100

    Last commit before NYSC; finished forecast route

--- main.go
+++ main.go
@@ -10,8 +10,11 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"strconv"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type CityStruct struct {
@@ -61,8 +64,54 @@ type ForecastSingleDayStruct struct {
 	Condition  ConditionStruct `json:"condition"`
 }
 
+type WeatherResponseForForecast struct {
+	Location CityStruct     `json:"location"`
+	Current  CurrentStruct  `json:"current"`
+	Forecast ForecastStruct `json:"forecast"`
+}
+
 // Database global variable
 var Database *sql.DB
+
+func main() {
+	ConnectDB()
+	var router *gin.Engine = gin.Default()
+
+	router.GET("/forecast", func(context *gin.Context) {
+		// context.JSON(200, gin.H{"message": "Hello, world"})
+		city := context.Query("city")
+		if city == "" || city == " " {
+			context.JSON(400, gin.H{
+				"status":  400,
+				"message": "Please provide a city name",
+				"error":   "Bad Request",
+			})
+			return
+		}
+		_, found, err := CheckLocationInDB(city)
+		if err != nil {
+			context.JSON(404, gin.H{
+				"status":  500,
+				"message": "Internal server error",
+				"error":   "Internal server error",
+			})
+			panic(err)
+		}
+		if found == false && err == nil {
+			context.JSON(404, gin.H{
+				"status":  404,
+				"message": "Could not find city in db",
+				"error":   "Not found",
+			})
+			return
+		}
+		response := FetchForecastDataFromDB(city)
+		fmt.Println(response)
+		context.JSON(200, response)
+	})
+
+	router.Run(":8080")
+}
 
 // Function to connect to database
 func ConnectDB() {
@@ -70,9 +119,9 @@ func ConnectDB() {
 		fmt.Println("Error occured while trying to load .env file")
 	}
 	db_host := os.Getenv("DB_HOST")
-	db_port := os.Getenv("DB_PORT")
+	db_port, _ := strconv.Atoi(os.Getenv("DB_PORT"))
 	db_user := os.Getenv("DB_USER")
-	db_name := os.Getenv("DB_USER")
+	db_name := os.Getenv("DB_NAME")
 	db_password := os.Getenv("DB_PASSWORD")
 
 	var uri string = fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable", db_host, db_port, db_user, db_name, db_password)
@@ -80,8 +129,70 @@ func ConnectDB() {
 	db, err := sql.Open("postgres", uri)
 	if err != nil {
 		fmt.Println("Error occured while connecting to db")
+		panic(err)
 	}
 	Database = db
 	fmt.Println("Database Connected Successfully")
 	// Databse connected successfully
+}
+
+func CheckLocationInDB(city string) (int, bool, error) {
+	var cityId int
+	err := Database.QueryRow(`SELECT id FROM location WHERE name=$1`, city).Scan(&cityId)
+
+	if err == sql.ErrNoRows {
+		fmt.Println("Could not find city in DB")
+		return 0, false, nil
+	}
+	if err != nil {
+		fmt.Println("An error occured while trying to access city id from DB")
+		return 0, false, err
+	}
+	return cityId, true, nil
+
+}
+
+func FetchForecastDataFromDB(cityID string) WeatherResponseForForecast {
+	var location CityStruct
+	var current CurrentStruct
+	var forecast ForecastStruct
+
+	// SELECT l.id, l.name, l.region, l.country, l.latitude, l.longitude, l.tz_id, id, city_id, last_updated, temp_c, condition_text, condition_icon, wind_mph, wind_degree, pressure_in, humidity FROM location l
+	// JOIN current c ON current.city_id=location.id
+	// WHERE name=$1
+	locationErr := Database.QueryRow(`SELECT id, name, region, country, latitude, longitude, tz_id FROM location WHERE name=$1`, cityID).Scan(
+		&location.ID, &location.Name, &location.Region, &location.Country, &location.Latitude, &location.Longitude, &location.TZ_ID,
+	)
+	currentErr := Database.QueryRow(`SELECT id, city_id, last_updated, temp_c, condition_text, condition_icon, wind_mph, wind_degree, pressure_in, humidity FROM current WHERE city_id=$1`, location.ID).Scan(
+		&current.ID, &current.CityID, &current.LastUpdated, &current.TempC, &current.Condition.Text, &current.Condition.Icon, &current.WindMph, &current.WindDegree, &current.PressureIn, &current.Humidity,
+	)
+	rows, rowsErr := Database.Query(`SELECT date, date_epoch, temperature, wind_mph, humidity, condition_text, condition_icon FROM forecast WHERE city_id=$1`, location.ID)
+	for rows.Next() {
+		var f ForecastDayArrStruct
+		rows.Scan(
+			&f.Date, &f.DateEpoch, &f.Day.TempC, &f.Day.MaxWindMph, &f.Day.Humidity, &f.Day.Condition.Text, &f.Day.Condition.Icon,
+		)
+		forecast.ForecastDay = append(forecast.ForecastDay, f)
+	}
+	if rowsErr != nil {
+		fmt.Println("City does not exist in DB: FetchForecastDataFromDB")
+	}
+	if locationErr == sql.ErrNoRows {
+		fmt.Println("City does not exist in DB: FetchForecastDataFromDB")
+	}
+	if locationErr != nil {
+		fmt.Println("An error occured while fetching from DB: FetchForecastDataFromDB")
+	}
+	if currentErr == sql.ErrNoRows {
+		fmt.Println("City does not exist in DB: FetchForecastDataFromDB")
+	}
+	if currentErr != nil {
+		fmt.Println("An error occured while fetching from DB: FetchForecastDataFromDB")
+	}
+	var response = WeatherResponseForForecast{
+		Location: location,
+		Current:  current,
+		Forecast: forecast,
+	}
+	return response
 }
